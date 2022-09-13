42426afcf7efe32d53def4b4b603d49cd858382f
Ivan Nenashev <inenashev@icloud.com>
2022-09-13 14:20:14 +0300

    app debug true

--- server.py
+++ server.py
@@ -3,11 +3,12 @@ app = Flask(__name__)
 from weather import weather_by_city
 
 
-@app.route("/")
-def hello():
-    return "Привет!"
+@app.route('/')
+def index():
+    weather = weather_by_city("Moscow,Russia")
+    return f"Сейчас {weather['temp_C']},ощущается как {weather['FeelsLikeC']}"
 
 if __name__=="__main__":
-    weather = weather_by_city("Moscow,Russia")
-    print(weather)
-    app.run()
+    #weather = weather_by_city("Moscow,Russia")
+    #print(weather)
+    app.run(debug=True)
