3abb86b6f4bcc442640cffb8afcf75422745f734
Ivan Nenashev <inenashev@icloud.com>
2022-09-13 14:23:43 +0300

    rollback debug true

--- server.py
+++ server.py
@@ -6,7 +6,10 @@ from weather import weather_by_city
 @app.route('/')
 def index():
     weather = weather_by_city("Moscow,Russia")
-    return f"Сейчас {weather['temp_C']},ощущается как {weather['FeelsLikeC']}"
+    if weather:
+        return f"Сейчас {weather['temp_C']},ощущается как {weather['FeelsLikeC']}"
+    else:
+        return "Прогноз сейчас недоступен"
 
 if __name__=="__main__":
     #weather = weather_by_city("Moscow,Russia")
