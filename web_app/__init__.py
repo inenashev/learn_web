665cc479e69ae6b4c2328bd1d171448ca4ad2347
Ivan Nenashev <inenashev@icloud.com>
2022-09-25 20:39:27 +0300

    forms

--- web_app/__init__.py
+++ web_app/__init__.py
@@ -2,6 +2,7 @@ from flask import Flask, render_template
 from web_app.weather import weather_by_city
 from web_app.python_org_news import get_python_news
 from web_app.model import db,News
+from web_app.forms import LoginForm
 
 def create_app():
 
@@ -15,5 +16,11 @@ def create_app():
         weather = weather_by_city(app.config['WEATHER_DEFAULT_CITY'])
         news_list = News.query.order_by(News.published.desc()).all()
         return render_template("index.html", page_title = page_title, weather=weather, news_list = news_list)
+
+    @app.route('/login')
+    def login():
+        title = "Авторизация"
+        login_form = LoginForm()
+        return render_template("login.html",page_title=title, form=login_form)
 
     return app
