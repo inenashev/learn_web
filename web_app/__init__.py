27ef9b833ecdcebe7e8c7a9324222652a0ecb901
Ivan Nenashev <inenashev@icloud.com>
2022-09-25 20:17:31 +0300

    add db

--- web_app/__init__.py
+++ web_app/__init__.py
@@ -1,7 +1,7 @@
 from flask import Flask, render_template
 from web_app.weather import weather_by_city
 from web_app.python_org_news import get_python_news
-from web_app.model import db
+from web_app.model import db,News
 
 def create_app():
 
@@ -13,7 +13,7 @@ def create_app():
     def index():
         page_title = 'новости python'
         weather = weather_by_city(app.config['WEATHER_DEFAULT_CITY'])
-        news_list = get_python_news()
+        news_list = News.query.order_by(News.published.desc()).all()
         return render_template("index.html", page_title = page_title, weather=weather, news_list = news_list)
 
     return app
