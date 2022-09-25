dc4bce6be4c26217dd64deddc3ce51e8355f4f13
Ivan Nenashev <inenashev@icloud.com>
2022-09-25 11:51:10 +0300

    Шаблоны

--- server.py
+++ server.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template
 from weather import weather_by_city
 
 
@@ -9,25 +9,9 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
+    page_title = 'Прогноз погоды'
     weather = weather_by_city("Moscow,Russia")
-    if weather:
-        weather_text = f"Сейчас {weather['temp_C']},ощущается как {weather['FeelsLikeC']}"
-    else:
-        weather_text = "Прогноз сейчас недоступен"
-    return f"""
-        <html>
-            <head>
-                <title> Прогноз погоды </title>
-            </head>
-            <body>
-                <h1> {weather_text} </h1>
-                <ol>
-                    <li>Один</li>
-                    <li>Два</li>
-                </ol>
-            </body>
-        </html>
-    """
+    return render_template("index.html", page_title = page_title, weather=weather)
 
 if __name__=="__main__":
     #weather = weather_by_city("Moscow,Russia")
