1f128a18f9821665bab626612b0b13df9b75d22a
Ivan Nenashev <inenashev@icloud.com>
2022-09-25 11:32:17 +0300

    debug still does not work

--- server.py
+++ server.py
@@ -2,7 +2,10 @@ from flask import Flask
 from weather import weather_by_city
 
 
+
 app = Flask(__name__)
+
+
 
 @app.route('/')
 def index():
@@ -15,4 +18,7 @@ def index():
 if __name__=="__main__":
     #weather = weather_by_city("Moscow,Russia")
     #print(weather)
+    #print(f"Token is: {token}")
+    #app.secret_key = 'super secret key'
+    #app.run(debug=True)
     app.run()
--- weather.py
+++ weather.py
@@ -1,24 +1,28 @@
 import requests
-import custom_secrets
+#import custom_secrets
 
 
 def weather_by_city(city_name):
     url = "http://api.worldweatheronline.com/premium/v1/weather.ashx"
 
     params = {
-        "key": custom_secrets.weather_token,
+        "key": "b8b1ac3aa0104a3da7684447221309", #custom_secrets.weather_token,
         "q": city_name,
         "format": "json",
         "num_of_days": 1,
         "lang": "ru"
         }
-
-    result = requests.get(url, params=params)
-    weather = result.json()
-    if 'data' in weather:
-        if 'current_condition' in weather['data']:
-            try:
-                return weather['data']['current_condition'][0]
-            except(IndexError, TypeError):
-                return False
+    try:
+        result = requests.get(url, params=params)
+        result.raise_for_status()
+        weather = result.json()
+        if 'data' in weather:
+            if 'current_condition' in weather['data']:
+                try:
+                    return weather['data']['current_condition'][0]
+                except(IndexError, TypeError):
+                    return False
+    except(requests.RequestException, ValueError):
+        print("network error")
+        return False
     return False
