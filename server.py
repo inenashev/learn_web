efc5e85c9ba6dc6631e16a865fcf49ce47904c74
Ivan Nenashev <inenashev@icloud.com>
2022-09-25 11:36:55 +0300

    html

--- server.py
+++ server.py
@@ -11,9 +11,23 @@ app = Flask(__name__)
 def index():
     weather = weather_by_city("Moscow,Russia")
     if weather:
-        return f"Сейчас {weather['temp_C']},ощущается как {weather['FeelsLikeC']}"
+        weather_text = f"Сейчас {weather['temp_C']},ощущается как {weather['FeelsLikeC']}"
     else:
-        return "Прогноз сейчас недоступен"
+        weather_text = "Прогноз сейчас недоступен"
+    return f"""
+        <html>
+            <head>
+                <title> Прогноз погоды </title>
+            </head>
+            <body>
+                <h1> {weather_text} </h1>
+                <ol>
+                    <li>Один</li>
+                    <li>Два</li>
+                </ol>
+            </body>
+        </html>
+    """
 
 if __name__=="__main__":
     #weather = weather_by_city("Moscow,Russia")
