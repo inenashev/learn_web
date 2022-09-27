4cf25d4d11f4e30cc0b562c5e351edea28ed789e
Ivan Nenashev <inenashev@icloud.com>
2022-09-27 18:17:59 +0300

    admin template

--- web_app/admin/views.py
+++ web_app/admin/views.py
@@ -1,12 +1,10 @@
-from flask import Blueprint
-from flask_login import current_user,login_required
-
+from flask import Blueprint,render_template
+from web_app.user.decorators import admin_required
 blueprint = Blueprint('admin',__name__,url_prefix="/admin")
 
-@blueprint.route('/admin')
-@login_required
+
+@blueprint.route('/')
+@admin_required
 def admin_index():
-    if current_user.is_admin:
-        return "Привет админ"
-    else:
-        return("ты не админ")
+    title = 'панель управления'
+    return render_template('admin/index.html', page_text=title)
